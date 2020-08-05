27360a1a93918b8ba53688f584763cb4dfd4241f
kiupets <gonzalogg1975@gmail.com>
2020-08-04 22:33:37 -0300

    improving scroll

--- src/components/CalendarContainer.js
+++ src/components/CalendarContainer.js
@@ -7,26 +7,26 @@ import CalendarContainerIndividual from './calendarContainerIndividual'
 const CalendarContainer = () => {
   const { state, nextMsg, prevMsg } = useContext(Context)
   const { calendarArray, current } = state
-  const [n, setN] = useState(0)
 
   const calendar = calendarArray.map((n) => {
     return <CalendarContainerIndividual month={calendarArrayy[n]} />
   })
-  useEffect(() => {
-    const scrollingDay = (e) => {
-      setN(window.scrollX)
-    }
-    window.addEventListener('scroll', scrollingDay)
-    if (n === 0) {
-      prevMsg(current)
-      window.scrollTo(3000, 0)
-    } else if (n > 5200) {
-      nextMsg(current)
-    }
-  }, [n])
+  const scrolling = (e) => {
+    const el = e.target
+    return el.scrollLeft === 0
+      ? console.log('hola')
+      : el.scrollWidth - el.scrollLeft === el.clientWidth
+      ? console.log('chau')
+      : null
+  }
 
   return (
-    <div style={{ display: 'grid', gridAutoFlow: 'column' }}>{calendar}</div>
+    <div
+      onScroll={scrolling}
+      style={{ overflowX: 'scroll', overflowY: 'hidden' }}
+    >
+      {calendar}
+    </div>
   )
 }
 
